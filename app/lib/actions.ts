4723d9d8a43379a84d81953f5bb340901ff5b20c
lucasfechio <lucasfechio94@gmail.com>
2024-12-17 12:03:30 -0300

    fix: type error

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -69,8 +69,7 @@ export async function deleteInvoice(id: string) {
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`
     revalidatePath('/dashboard/invoices')
-    return { message: 'Deleted Invoice.' }
   } catch (error) {
-    return { message: 'Database Error: Failed to Delete Invoice.' }
+    throw new Error('Failed to Delete Invoice')
   }
 }
